7861f4a665cc91fd479999ae2e15d7cd9688716c
Hossam Maher <hossam@Hossams-MacBook-Pro.local>
2021-01-26 06:52:15 +0200

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'email';
+  email = '';
+
+  constructor() {
+    // setInterval(() => {
+    //   this.email += 'a';
+    // }, 1000);
+  }
+
+  onSubmit() {
+    console.log(this.email);
+  }
+
+  log() {
+    console.log('hi');
+  }
 }
